7489add77d7cfba73243b8e7757e6d657a5efd15
jcarnahan-hub <jcarnahan78@gmail.com>
2026-05-09 08:12:38 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'sageechoes-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/firebase.js',
+  '/db.js',
+  '/import.js',
+  '/backup.js',
+  '/books-api.js',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
